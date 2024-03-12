34d8dd4294b7ce7cece2d91c998e383a3cf33125
seanavery <seanpollock3344@gmail.com>
2024-03-12 16:47:03 -0400

    Fix

--- decoder.go
+++ decoder.go
@@ -7,7 +7,7 @@ import (
 )
 
 /*
-#cgo LDFLAGS: -lavformat -lavcodec -lavfilter -lavutil -lswresample -lswscale -lz -lm
+#cgo LDFLAGS: -lavformat -lavcodec -lavfilter -lavutil -lswresample -lswscale -lm
 #include <libavcodec/avcodec.h>
 #include <libavutil/imgutils.h>
 #include <libswscale/swscale.h>
